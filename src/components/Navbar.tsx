96012cc10f5a158dc0a61022a5c497d7e02dc6ed
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-01-02 18:34:50 +0530

    Completed read by expense id

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,50 +14,67 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <div className="navbar-nav">
                         {/* Link to the Dashboard page */}
-                        <NavLink className="nav-link" to="/">
+                        <NavLink
+                            className={({ isActive }) =>
+                                `nav-link ${isActive ? "active" : ""}`
+                            }
+                            to="/"
+                        >
                             Dashboard
                         </NavLink>
 
                         {/* Link to the New Expense page */}
-                        <NavLink className="nav-link" to="/new">
+                        <NavLink
+                            className={({ isActive }) =>
+                                `nav-link ${isActive ? "active" : ""}`
+                            }
+                            to="/new"
+                        >
                             New Expense
                         </NavLink>
 
                         {/* Link to the Reports page */}
-                        <NavLink className="nav-link" to="/reports">
+                        <NavLink
+                            className={({ isActive }) =>
+                                `nav-link ${isActive ? "active" : ""}`
+                            }
+                            to="/reports"
+                        >
                             Reports
                         </NavLink>
                     </div>
                 </div>
-            </div>
 
-            {/* Right-side buttons: Login and Register */}
-            <div className="d-flex" role="search">
-                {/* Link to the Login page */}
-                <NavLink className="btn stn-sm btn-outline-light" to="/login">
-                    Login
-                </NavLink>
+                {/* Right-side buttons: Login and Register */}
+                <div className="d-flex" role="search">
+                    {/* Link to the Login page */}
+                    <NavLink
+                        className="btn btn-sm btn-outline-light"
+                        to="/login"
+                    >
+                        Login
+                    </NavLink>
 
-                {/* Link to the Register page */}
-                <NavLink
-                    className="btn stn-sm btn-outline-light mx-1"
-                    to="/register"
-                >
-                    Register
-                </NavLink>
-
-                {/* Toggler button for collapsing the navbar on smaller screens */}
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <FaBars color="white" />
-                </button>
+                    {/* Link to the Register page */}
+                    <NavLink
+                        className="btn btn-sm btn-outline-light mx-1"
+                        to="/register"
+                    >
+                        Register
+                    </NavLink>
+                    {/* Toggler button for collapsing the navbar on smaller screens */}
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <FaBars color="white" />
+                    </button>
+                </div>
             </div>
         </nav>
     );
